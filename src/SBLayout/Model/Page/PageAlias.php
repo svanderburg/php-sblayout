a570db3102fbdb31e6ed207545b8fe9147efe4f7
Sander van der Burg <svanderburg@gmail.com>
2017-08-03 23:53:42 +0200

    Fix problem with exception

--- src/SBLayout/Model/Page/PageAlias.php
+++ src/SBLayout/Model/Page/PageAlias.php
@@ -1,7 +1,7 @@
 <?php
 namespace SBLayout\Model\Page;
 use SBLayout\Model\Application;
-
+use SBLayout\Model\PageNotFoundException;
 /**
  * Defines a page alias that is an alias of an existing page that
  * can be reached from the entry page.
