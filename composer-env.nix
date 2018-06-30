44493847ebf631b52cf81607e2eb227c65390acb
Sander van der Burg <svanderburg@gmail.com>
2018-06-30 15:56:25 +0200

    Update composer2nix expressions

--- composer-env.nix
+++ composer-env.nix
@@ -2,7 +2,7 @@
 
 { stdenv, writeTextFile, fetchurl, php, unzip }:
 
-rec {
+let
   composer = stdenv.mkDerivation {
     name = "composer-1.6.5";
     src = fetchurl {
@@ -49,7 +49,21 @@ rec {
       '';
     };
 
-  buildPackage = { name, src, packages ? {}, devPackages ? {}, symlinkDependencies ? false, executable ? false, removeComposerArtifacts ? false, postInstall ? "", noDev ? false, ...}@args:
+  buildPackage =
+    { name
+    , src
+    , packages ? {}
+    , devPackages ? {}
+    , buildInputs ? []
+    , symlinkDependencies ? false
+    , executable ? false
+    , removeComposerArtifacts ? false
+    , postInstall ? ""
+    , noDev ? false
+    , unpackPhase ? "true"
+    , buildPhase ? "true"
+    , ...}@args:
+
     let
       reconstructInstalled = writeTextFile {
         name = "reconstructinstalled.php";
@@ -149,11 +163,16 @@ rec {
               }
             ''}
           '') (builtins.attrNames dependencies);
+
+      extraArgs = removeAttrs args [ "name" "packages" "devPackages" "buildInputs" ];
     in
-    stdenv.lib.makeOverridable stdenv.mkDerivation (builtins.removeAttrs args [ "packages" "devPackages" ] // {
-      name = "composer-${args.name}";
-      buildInputs = [ php composer ] ++ args.buildInputs or [];
-      buildCommand = ''
+    stdenv.mkDerivation ({
+      name = "composer-${name}";
+      buildInputs = [ php composer ] ++ buildInputs;
+
+      inherit unpackPhase buildPhase;
+
+      installPhase = ''
         ${if executable then ''
           mkdir -p $out/share/php
           cp -av $src $out/share/php/$name
@@ -242,5 +261,10 @@ rec {
         # Execute post install hook
         runHook postInstall
     '';
-  });
+  } // extraArgs);
+in
+{
+  composer = stdenv.lib.makeOverridable composer;
+  buildZipPackage = stdenv.lib.makeOverridable buildZipPackage;
+  buildPackage = stdenv.lib.makeOverridable buildPackage;
 }
